9e5c9d3ab70762c1f010f5591653e927ee30d7a9
Audrey Carlsen <acarlsen@seattletimes.com>
2016-01-07 13:08:30 -0800

    adds interaction

--- src/js/main.js
+++ src/js/main.js
@@ -3,4 +3,15 @@
 // var track = require("./lib/tracking");
 
 require("component-responsive-frame/child");
+var dot = require("./lib/dot");
 
+var template = dot.compile(require("./_info.html"));
+
+var qsa = s => Array.prototype.slice.call(document.querySelectorAll(s));
+
+qsa(".st-group").forEach(function(group) {
+  group.addEventListener("click", function(e) {
+    
+    document.querySelector(".info").innerHTML = template(data[e.target.parentElement.id]);
+  });
+});
