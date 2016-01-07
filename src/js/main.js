ceefa99ca79f8e99f14a756d7682724ee8962ed5
Audrey Carlsen <acarlsen@seattletimes.com>
2016-01-07 13:15:01 -0800

    adds selected styling

--- src/js/main.js
+++ src/js/main.js
@@ -11,7 +11,8 @@ var qsa = s => Array.prototype.slice.call(document.querySelectorAll(s));
 
 qsa(".st-group").forEach(function(group) {
   group.addEventListener("click", function(e) {
-    
     document.querySelector(".info").innerHTML = template(data[e.target.parentElement.id]);
+    if (document.querySelector(".selected")) document.querySelector(".selected").classList.remove("selected");
+    e.target.parentElement.classList.add("selected");
   });
 });
